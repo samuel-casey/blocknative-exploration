d5739978785c72434fb03f74904fd54888194780
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2021-01-18 20:14:12 -0500

    added configuration for SDK

--- blocknative-sdk/src/App.tsx
+++ blocknative-sdk/src/App.tsx
@@ -1,17 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Web3 from 'web3';
 import blocknativeSDK from 'bnc-sdk';
-import Loader from 'react-loader-spinner';
+import configuration from './configuration.json'
+import sdkSetup from './sdkSetup'
+// import Loader from 'react-loader-spinner';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 import './App.css';
 
-const DAPP_ID: string = '1e6835da-e541-4da6-bec2-e5047dc0031e'
-const NETWORK_ID: number = 4
+// const DAPP_ID: string = 'ce6489a0-beeb-4c0a-99ff-d168118b35e5' // API KEY FOR BN ACCOUNT
+const NETWORK_ID: number = 1 // mainnet
+
+const logTransactionEvent = (event: any) => console.log("event:", event.transaction)
 
 const blocknative = new blocknativeSDK({
   dappId: DAPP_ID,
-  networkId: NETWORK_ID
+  networkId: NETWORK_ID,
+  // transactionHandlers: [logTransactionEvent]
 })
+
+sdkSetup(blocknative, configuration)
 
 // const { clientIndex } = blocknative
 
@@ -20,8 +27,7 @@ declare const window: any;
 const provider = window.ethereum
 const web3 = new Web3(provider)
 
-const rinkelborg = '0xD3a4CCF97122DA15750E50d3E97B54DF1D71DAA5'
-const runklebug = '0x11113c2fba9ae08d7d16c817f03de51f29117db2'
+const USDC_ETH_ADDRESS = '0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc'
 
 interface Itx {
   hash: string;
@@ -29,70 +35,32 @@ interface Itx {
 
 const App = (): JSX.Element => {
 
+  const {emitter, details} =  blocknative.account(USDC_ETH_ADDRESS)
+
   const [txs, setTxs] = useState<Itx[] | any>([])
-  const [status, setStatus] = useState<any>()
+  const [valIncoming, setValIncoming] = useState(0)
 
-  const sendTx = async (e: any): Promise<any> => {
-    e.preventDefault();
-    const accounts = await web3.eth.getAccounts()
-    const address = accounts[0]
-
-    const txOptions = {
-      to: runklebug,
-      from: address,
-      value: web3.utils.toWei("0.00000001")
-    }
-
-    web3.eth.sendTransaction(txOptions)
-      .on('transactionHash', hash => {
-        const { emitter } = blocknative.transaction(hash)
-
-        emitter.on('txPool', transaction => {
-          console.log(transaction)
-          console.log('tx in mempool')
-          setStatus('pending')
-        })
-
-        emitter.on('txConfirmed', (transaction: any) => {
-          console.log(transaction)
-          console.log('tx confirmed')
-          setTxs((prevTxs: any) => [...prevTxs, { hash: transaction.hash, value: transaction.value }])
-          setStatus('complete')
-        })
-      })
-  }
 
   const txData = txs ? txs.map((tx: any) => <><p key={tx.hash}>tx: {tx.hash}: {web3.utils.fromWei(tx.value)} ether</p></>) : null;
 
+  useEffect(() => {
 
-  let txStatus;
-
-  switch (status) {
-    case 'pending':
-      txStatus = <Loader type="Puff"
-        color="#00BFFF"
-        height={100}
-        width={100}
-        timeout={3000} />
-      break;
-    case 'complete':
-      const lastTx = txs.length > 0 ? txs.slice(txs.length - 1, 1)[0].hash : 'womp'
-      console.log(lastTx)
-      txStatus = <p>TX {lastTx} COMPLETE</p>
-      break;
-    default:
-      txStatus = 'Waiting for tx to be sent via MetaMask'
-      break;
-  }
-
+    emitter.on("txConfirmed", (transaction: any) => {
+      console.log('emitter: ')
+      console.log(transaction)
+      setTxs([...txs, transaction])
+      if (transaction.direction !== "") console.log(transaction.direction)
+      const ether = +web3.utils.fromWei(transaction.value)
+      setValIncoming(valIncoming + ether)
+    })
+  }, [txs])
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <button onClick={sendTx}>Send Transaction</button>
-      </header>
       <main>
-        <div>{txStatus}</div>
         <h3>Transactions</h3>
+        <p>{valIncoming}</p>
         <div>{txData}</div>
       </main>
     </div>
---
+++ blocknative-sdk/src/SendTxApp.tsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import Web3 from 'web3';
+import blocknativeSDK from 'bnc-sdk';
+import Loader from 'react-loader-spinner';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import './App.css';
+
+const DAPP_ID: string = 'ce6489a0-beeb-4c0a-99ff-d168118b35e5' // API KEY FOR BN ACCOUNT
+const NETWORK_ID: number = 4
+
+const blocknative = new blocknativeSDK({
+  dappId: DAPP_ID,
+  networkId: NETWORK_ID
+})
+
+// const { clientIndex } = blocknative
+
+declare const window: any;
+
+const provider = window.ethereum
+const web3 = new Web3(provider)
+
+const rinkelborg = '0xD3a4CCF97122DA15750E50d3E97B54DF1D71DAA5'
+const runklebug = '0x11113c2fba9ae08d7d16c817f03de51f29117db2'
+
+interface Itx {
+  hash: string;
+}
+
+const App = (): JSX.Element => {
+
+  const [txs, setTxs] = useState<Itx[] | any>([])
+  const [status, setStatus] = useState<any>()
+
+  const sendTx = async (e: any): Promise<any> => {
+    e.preventDefault();
+    const accounts = await web3.eth.getAccounts()
+    const address = accounts[0]
+
+    const txOptions = {
+      to: runklebug,
+      from: address,
+      value: web3.utils.toWei("0.00000001")
+    }
+
+    web3.eth.sendTransaction(txOptions)
+      .on('transactionHash', hash => {
+        const { emitter } = blocknative.transaction(hash)
+
+        emitter.on('txPool', transaction => {
+          console.log(transaction)
+          console.log('tx in mempool')
+          setStatus('pending')
+        })
+
+        emitter.on('txConfirmed', (transaction: any) => {
+          console.log(transaction)
+          console.log('tx confirmed')
+          setTxs((prevTxs: any) => [...prevTxs, { hash: transaction.hash, value: transaction.value }])
+          setStatus('complete')
+        })
+      })
+  }
+
+  const txData = txs ? txs.map((tx: any) => <><p key={tx.hash}>tx: {tx.hash}: {web3.utils.fromWei(tx.value)} ether</p></>) : null;
+
+
+  let txStatus;
+
+  switch (status) {
+    case 'pending':
+      txStatus = <Loader type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} />
+      break;
+    case 'complete':
+      const lastTx = txs.length > 0 ? txs.slice(txs.length - 1, 1)[0].hash : 'womp'
+      console.log(lastTx)
+      txStatus = <p>TX {lastTx} COMPLETE</p>
+      break;
+    default:
+      txStatus = 'Waiting for tx to be sent via MetaMask'
+      break;
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={sendTx}>Send Transaction</button>
+      </header>
+      <main>
+        <div>{txStatus}</div>
+        <h3>Transactions</h3>
+        <div>{txData}</div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
