a63121c2a700c1c58cb4e4ac66bc69db487bff77
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2021-01-19 21:18:42 -0500

    add more addresses placeholder

--- blocknative-sdk/src/addresses.js
+++ blocknative-sdk/src/addresses.js
@@ -1,5 +1,9 @@
 export const addresses = {
 	uniswap: {
 		USDC_ETH: '0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc',
+		WBTC_ETH: '0xbb2b8038a1640196fbe3e38816f3e67cba72d940',
 	},
+	compound: {},
+	aave: {},
+	sushiswap: {},
 };
