d0ff10e4433811d20a54a54a3e7070c1a73ad028
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2021-01-23 21:57:45 -0500

    addded abi and address for cUSDT

--- blocknative-sdk/src/addresses.js
+++ blocknative-sdk/src/addresses.js
@@ -3,6 +3,9 @@ export const addresses = {
 		USDC_ETH: '0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc',
 		WBTC_ETH: '0xbb2b8038a1640196fbe3e38816f3e67cba72d940',
 	},
+	compound: {
+		cUSDT: '0xf650c3d88d12db855b8bf7d11be6c55a4e07dcc9'
+	}
 	compound: {},
 	aave: {},
 	sushiswap: {},
