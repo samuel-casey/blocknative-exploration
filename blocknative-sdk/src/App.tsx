6b5a8bfda289f1cee92433bf13c238f6c0c948a3
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2021-01-25 21:07:32 -0500

    removed test getIncomingValue call from useEffect

--- blocknative-sdk/src/App.tsx
+++ blocknative-sdk/src/App.tsx
@@ -85,48 +85,11 @@ const App = (): JSX.Element => {
   const txData = txs ? txs.map((tx: any) => <><p key={tx.hash}>tx: {tx.hash}: {web3.utils.fromWei(tx.value)} ether</p></>) : null;
 
   useEffect(() => {
-
-//     console.log(getIncomingValue({
-//   "status": "confirmed",
-//   "monitorId": "GETH_1_F_PROD",
-//   "monitorVersion": "0.79.3",
-//   "timePending": "360330",
-//   "blocksPending": 31,
-//   "pendingTimeStamp": "2021-01-25T20:39:44.274Z",
-//   "pendingBlockNumber": 11727106,
-//   "hash": "0x4dccd0035d676494549097d587f17ff7613f8346592d696df6ff902c52c6b766",
-//   "from": "0x51E646cf615DF72c6732EE407f6F524DD4374E45",
-//   "to": "0xf650C3d88D12dB855b8bf7D11Be6C55A4e07dCC9",
-//   "value": "0",
-//   "gas": 242020,
-//   "gasPrice": "50000000000",
-//   "nonce": 11,
-//   "blockHash": "0x949218f7e2b39898316b5e53d6172bb799be88f4d984686d1831c1ac1aa5d78e",
-//   "blockNumber": 11727137,
-//   "transactionIndex": 118,
-//   "input": "0x852a12e30000000000000000000000000000000000000000000000000000000000b71b00",
-//   "gasUsed": "189981",
-//   "asset": "ETH",
-//   "blockTimeStamp": "2021-01-25T20:45:40.000Z",
-//   "watchedAddress": "0xf650c3d88d12db855b8bf7d11be6c55a4e07dcc9",
-//   "direction": "incoming",
-//   "counterparty": "0x51E646cf615DF72c6732EE407f6F524DD4374E45",
-//   "id": "0x4dccd0035d676494549097d587f17ff7613f8346592d696df6ff902c52c6b766",
-//   "serverVersion": "0.87.4",
-//   "eventCode": "txConfirmed",
-//   "timeStamp": "2021-01-25T20:45:44.580Z",
-//   "system": "ethereum",
-//   "network": "main"
-// }))
-    
-    
-    
-    /// @notice functions to handle pending and txConfirmed events
-
-    // need to handle transaction fail
-    
+        
+    /// @notice functions to handle various mempool events
     emitter.on("txPool", (transaction: any) => getIncomingValue(transaction))
-
+    
+    // need to handle transaction fail
     emitter.on("txFailed", (transaction: any) =>  {
       if (txs.includes(transaction)) {
         const newTxs = txs.filter((tx: any) => tx.hash === transaction.hash)
