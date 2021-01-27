c477f211f098236dfa9de03169760ef64316ad71
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2021-01-26 20:10:59 -0500

    added more functions for decoding values and some considerations for different method names

--- blocknative-sdk/src/App.tsx
+++ blocknative-sdk/src/App.tsx
@@ -11,6 +11,14 @@ import {addresses} from './addresses'
 const USDC_ETH = addresses.uniswap.USDC_ETH
 const cUSDT = addresses.compound.cUSDT
 
+
+// typescript stuffs
+//  enum valueType {
+//     pending = "pending",
+//     confirmed = "confirmed"
+//   }
+ type TTransaction = any
+
 /// @dev test
 // const DAPP_ID: string = 'ce6489a0-beeb-4c0a-99ff-d168118b35e5' // API KEY #1 FOR BN ACCOUNT
 /// @dev test2
@@ -48,19 +56,36 @@ declare const window: any;
 const provider = window.ethereum
 const web3 = new Web3(provider)
 
-interface Itx {
-  hash: string;
-}
-
 const App = (): JSX.Element => {
 
   const {emitter, details} =  blocknative.account(cUSDT)
 
-  const [txs, setTxs] = useState<Itx[] | any>([])
+  const [pendingTxs, setPendingTxs] = useState<TTransaction | undefined>([])
+  const [confirmedTxs, setConfirmedTxs] = useState<TTransaction | undefined>([])
   const [valIncoming, setValIncoming] = useState(0)
   const [valConfirmed, setValConfirmed] = useState(0)
 
-  const decodeMintAmount = (token: string, input: string): number => {
+  const txHashesSet = new Set()
+
+  // remove pending tx from list of all txs if confirmed tx === a tx in pending list 
+  const addOrRemoveNewTx = (transaction: TTransaction) => {
+    if (transaction.status === 'pending') {
+      if (txHashesSet.has(transaction.hash)) {
+        // remove tx from current txs State
+        const newTxs = pendingTxs.filter((tx: TTransaction) => tx.hash === transaction.hash)
+        // set new State
+        setPendingTxs(newTxs)
+      } else {
+        txHashesSet.add(transaction.hash)
+        setPendingTxs([...pendingTxs, transaction])
+      }
+    } else if (transaction.status === "confirmed") {
+      // add tx to txs State if it's confirmed
+      setConfirmedTxs([...confirmedTxs, transaction])
+    }
+  }
+
+  const decodeTxAmount = (token: string, input: string): number => {
     // decode input 
     const value = +web3.eth.abi.decodeParameter('uint256', input)
     switch (token) {
@@ -70,57 +95,94 @@ const App = (): JSX.Element => {
         return value
     }    
   }
+  
+  const updateValueSums = (tokenType: string, valueType: string, transaction: TTransaction) => {    
+    // find hex of value being transferred by slicing out part of transaction.input that indicates the method name
+    const hexInput = transaction.input.slice(10, transaction.input.length)
+    console.log('tx input', hexInput)
 
-  const getIncomingValue = (transaction: any): void => {
-    setTxs([...txs, transaction])
-      const input = '0x' + transaction.input.slice(10, transaction.input.length)
-      console.log('txP input', input)
-      const value = decodeMintAmount('cUSDT', input)
-      console.log('value txP', value)
-      console.log('pending: ', transaction)
-      setValIncoming(valIncoming + value)
+    // decode amount being used in transaction
+    const decodedValue = decodeTxInput(transaction, tokenType)
+
+    if (valueType === "pending") {
+      if (!txHashesSet.has(transaction.hash)) {
+        setValIncoming(valIncoming + decodedValue)
+      }
+    } else if (valueType === "confirmed") {
+      if (txHashesSet.has(transaction.hash)) {
+        setValIncoming(valIncoming - decodedValue)
+        setValConfirmed(valConfirmed + decodedValue)
+      } else {
+        setValConfirmed(valConfirmed + decodedValue)
+      }
+    }
+
+    console.log('tx value', decodedValue)
   }
 
-  const txData = txs ? txs.map((tx: any) => <><p key={tx.hash}>tx: {tx.hash}: {web3.utils.fromWei(tx.value)} ether</p></>) : null;
+  const decodeTxInput = (transaction: TTransaction, token: string): number => {
+    // log status of tx
+    console.log(transaction.status, transaction)
+
+    const methodHex = transaction.input.slice(0,10)
+    const methodInput = transaction.input.slice(10, transaction.input.length)
+    
+    // log code of method
+    console.log('method hex:', methodHex)
+    console.log('method input:', methodInput)
+
+    // find method being called by slicing it out of transaction.input
+    switch (methodHex) {
+      case '0xa0712d68':
+        console.log('mint method called')
+        break;
+      case '0xc5ebeaec':
+        console.log('borrow method called')
+        break;
+      default:
+        console.log('idk method called')
+        break;
+    }
+
+  return decodeTxAmount(token, methodInput)
+  }
+  const pendingTxData = pendingTxs ? pendingTxs.map((tx: any) => <><p key={tx.hash}>tx: {tx.hash}: {web3.utils.fromWei(tx.value)} ether</p></>) : null;
+  
+  const confirmedTxData = confirmedTxs ? confirmedTxs.map((tx: any) => <><p key={tx.hash}>tx: {tx.hash}: {web3.utils.fromWei(tx.value)} ether</p></>) : null;
 
   useEffect(() => {
         
     /// @notice functions to handle various mempool events
-    emitter.on("txPool", (transaction: any) => getIncomingValue(transaction))
+    emitter.on("txPool", (transaction: TTransaction) => {
+      addOrRemoveNewTx(transaction)
+      updateValueSums("cUSDT", "pending", transaction)  
+    })
     
     // need to handle transaction fail
-    emitter.on("txFailed", (transaction: any) =>  {
-      if (txs.includes(transaction)) {
-        const newTxs = txs.filter((tx: any) => tx.hash === transaction.hash)
-        setTxs(newTxs)
-      }
+    emitter.on("txFailed", (transaction: TTransaction) =>  {
+      console.log(transaction.status, transaction)
+      addOrRemoveNewTx(transaction)
     })
 
-    emitter.on("txConfirmed", (transaction: any) => {
-      if (txs.includes(transaction)) {
-        const newTxs = txs.filter((tx: any) => tx.hash === transaction.hash)
-        setTxs(newTxs)
-      }
-      const input = '0x' + transaction.input.slice(10, transaction.input.length)
-      console.log('txC input', input)
-      const value = decodeMintAmount('cUSDT', input)
-      console.log('value txC', value)
-      console.log('pending: ', transaction)
-      setValConfirmed(valConfirmed + value)
-      console.log('confirmed: ', transaction)
+    emitter.on("txConfirmed", (transaction: TTransaction) => {
+      addOrRemoveNewTx(transaction)
+      updateValueSums("cUSDT", "confirmed", transaction)
     })
-  }, [txs])
-// }, [])
+  }, [pendingTxs, confirmedTxs])
 
   
   
   return (
     <div className="App">
       <main>
-        <h3>Transactions</h3>
+        <h1>DeFlow</h1>
+        <h2>Values</h2>
         <p>incoming: {valIncoming}</p>
         <p>confirmed: {valConfirmed}</p>
-        <div>{txData}</div>
+        <h2>Pending Tx Hashes</h2>
+        <div>{pendingTxData}</div>
+        <h2>Confirmed Tx Hashes</h2>
+        <div>{confirmedTxData}</div>
       </main>
     </div>
   );
