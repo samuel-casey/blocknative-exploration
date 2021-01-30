c35d19101648445acf8ba43ba89108e5d9931139
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2021-01-30 11:03:48 -0500

    added all compound addresses

--- blocknative-sdk/src/App.tsx
+++ blocknative-sdk/src/App.tsx
@@ -8,11 +8,10 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 import './App.css';
 import {addresses} from './addresses'
 import cUSDT_ABI from './cUSDT_ABI.json'
+import LoanShifterTaker_ABI from './LoanShifterTaker_ABI.json'
+import DSProxy_ABI from './DSProxy_ABI.json'
 
 const cUSDT = addresses.compound.cUSDT
-
-const e_provider = ethers.getDefaultProvider();
-const inter = new ethers.utils.Interface(cUSDT_ABI);
 
 // typescript stuffs
 type TTransaction = any
@@ -46,66 +45,58 @@ const App = (): JSX.Element => {
   const [confirmedTxs, setConfirmedTxs] = useState<TTransaction | undefined>([])
   const [valIncoming, setValIncoming] = useState(0)
   const [valConfirmed, setValConfirmed] = useState(0)
+  // const [abi, setAbi] = useState(LoanShifterTaker_ABI)
+  // const [abi, setAbi] = useState(cUSDT_ABI)
+  const [abi, setAbi] = useState(DSProxy_ABI)
+
+  const e_provider = ethers.getDefaultProvider();
+  const inter = new ethers.utils.Interface(abi);
 
   const txHashesSet = new Set()
 
-  const decodeTxInput = () => {
-
-  }
   const pendingTxData = pendingTxs ? pendingTxs.map((tx: any) => <><p key={tx.hash}>tx: {tx.hash}: {web3.utils.fromWei(tx.value)} ether</p></>) : null;
   
   const confirmedTxData = confirmedTxs ? confirmedTxs.map((tx: any) => <><p key={tx.hash}>tx: {tx.hash}: {web3.utils.fromWei(tx.value)} ether</p></>) : null;
 
  // ETHERS DECODE FUNCTION
- const decodeTxnValue = (async(txnHash: any) => {
-      const tx = await e_provider.getTransaction(txnHash);
+ const decodeTxnValue = (async(txnHash: any, abi: any) => {  
+    
+    // get txn using ethers provider
+    const tx = await e_provider.getTransaction(txnHash);
 
-        console.log(tx.data)
-
+        // decode input of txn
         const decodedInput = inter.parseTransaction({ data: tx.data, value: tx.value});
+
+        console.log(decodedInput)
 
         // Decoded Transaction
         return {
             function_name: decodedInput.name,
+            sighash: decodedInput.sighash,
             from: tx.from,
             to: decodedInput.args[0],
             erc20Value: Number(decodedInput.args[0])
           };        
     });
 
-    const callDecodeFunction = async (txnHash: any) => {
-      const result = await decodeTxnValue(txnHash)
-      console.log(txnHash, result.erc20Value)
+    const callDecodeFunction = async (txnHash: any, abi: any) => {
+      const result = await decodeTxnValue(txnHash, abi)
+      console.dir(result)
     }
 
+  const decodeTxInput = () => {
+    // main transaction function call (execute)
+    callDecodeFunction('0x853eaedce195464f407ca9c51825ae20995265bf34dfbb1076c482f86f4c7b8c', LoanShifterTaker_ABI)
+    
+    // first delegate call
+    // callDecodeFunction()
+  }
 
   useEffect(() => {
-     
-    console.log('basic mint')
-    callDecodeFunction('0x442d693bd5a1188c2b60d984feb42d3aedd2272cc11fb1e3aceed79e76f0bd9a')
-    
-    console.log('basic borrow')
-    callDecodeFunction('0x2922821ce0afe1ca0b076f93bdf76063858b60490e6533952c9e33950d1c73cf')
-    
-    console.log('1-inch')
-    callDecodeFunction('0x2d28c2d546142cd565f23ace7672e75b965630653d4859db1c6a3e70f29fea3d')
+    decodeTxInput()
 
-    // /// @notice functions to handle various mempool events
-    // emitter.on("txPool", (transaction: TTransaction) => {
-    //   callDecodeFunction(transaction.hash)
-    // })
-    
-    // // need to handle transaction fail
-    // emitter.on("txFailed", (transaction: TTransaction) =>  {
-    //   //   console.log(transaction.status, transaction)
-    //   callDecodeFunction(transaction.hash)
-    // })
-
-    // emitter.on("txConfirmed", (transaction: TTransaction) => {
-    // })
-  }, [pendingTxs, confirmedTxs])
-
-  
+    // callDecodeFunction('0x442d693bd5a1188c2b60d984feb42d3aedd2272cc11fb1e3aceed79e76f0bd9a', cUSDT_ABI)
+  }, [])
   
   return (
     <div className="App">
@@ -124,3 +115,30 @@ const App = (): JSX.Element => {
 }
 
 export default App;
+
+
+// useEffect(() => {
+     
+//   // console.log('basic mint')
+//   // callDecodeFunction('0x442d693bd5a1188c2b60d984feb42d3aedd2272cc11fb1e3aceed79e76f0bd9a')
+    
+//   // console.log('basic borrow')
+//   // callDecodeFunction('0x2922821ce0afe1ca0b076f93bdf76063858b60490e6533952c9e33950d1c73cf')
+    
+//   // console.log('1-inch')
+//   // callDecodeFunction('0x2d28c2d546142cd565f23ace7672e75b965630653d4859db1c6a3e70f29fea3d')
+
+//   // /// @notice functions to handle various mempool events
+//   // emitter.on("txPool", (transaction: TTransaction) => {
+//   //   callDecodeFunction(transaction.hash)
+//   // })
+    
+//   // // need to handle transaction fail
+//   // emitter.on("txFailed", (transaction: TTransaction) =>  {
+//   //   //   console.log(transaction.status, transaction)
+//   //   callDecodeFunction(transaction.hash)
+//   // })
+
+//   // emitter.on("txConfirmed", (transaction: TTransaction) => {
+//   // })
+// }, [pendingTxs, confirmedTxs])
--- blocknative-sdk/src/addresses.js
+++ blocknative-sdk/src/addresses.js
@@ -1,11 +1,15 @@
 export const addresses = {
-	uniswap: {
-		USDC_ETH: '0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc',
-		WBTC_ETH: '0xbb2b8038a1640196fbe3e38816f3e67cba72d940',
-	},
 	compound: {
+		cBAT: '0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e',
+		cCOMP: '0x70e36f6bf80a52b3b46b3af8e106cc0ed743e8e4',
+		cDAI: '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643',
+		cETH: '0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5',
+		cREP: '0x158079ee67fce2f58472a96584a73c7ab9ac95c1',
+		cSAI: '0xf5dce57282a584d2746faf1593d3121fcac444dc',
+		cUNI: '0x35a18000230da775cac24873d00ff85bccded550',
+		cUSDC: '0x39aa39c021dfbae8fac545936693ac917d5e7563',
 		cUSDT: '0xf650c3d88d12db855b8bf7d11be6c55a4e07dcc9',
+		cWBTC: '0xc11b1268c1a384e55c48c2391d8d480264a3a7f4',
+		cZRX: '0xb3319f5d18bc0d84dd1b4825dcde5d5f7266d407',
 	},
-	aave: {},
-	sushiswap: {},
 };
